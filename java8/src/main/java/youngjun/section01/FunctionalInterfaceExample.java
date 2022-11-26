e72626534acd125235ebf074084b8f89da17ff6d
YoungJun Park <youngjun108059@gmail.com>
2022-11-26 23:59:37 +0900

    [Java8] refactoring

--- java8/src/main/java/youngjun/section01/FunctionalInterfaceExample.java
+++ java8/src/main/java/youngjun/section01/FunctionalInterfaceExample.java
@@ -1,7 +1,8 @@
- package youngjun.section01;
+package youngjun.section01;
 
 /**
- * 함수형 인터페이스(추상 메서드 하나만 존재)
+ * 함수형 인터페이스 : 추상 메서드가 하나만 존재한다
+ * <p>
  * 정적 메서드 or 기본 메서드를 가질 수 있음
  */
 @FunctionalInterface
@@ -13,14 +14,14 @@ public interface FunctionalInterfaceExample {
     void doIt();
 
     /**
-     * 정적 메서드 가능(java8에서 추가)
+     * 정적 메서드 정의 가능(java8에서 추가)
      */
     static void printName() {
         System.out.println("YoungJun");
     }
 
     /**
-     * 기본 메서드 가능(java8에서 추가)
+     * 기본 메서드 정의 가능(java8에서 추가)
      */
     default void printAge() {
         System.out.println("28");
